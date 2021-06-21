682c9f3aa936db5927a6e3b24ed8eca4c9035fb1
Willie Chalmers III <willie.chalmers@acmutd.co>
2021-06-21 10:14:43 -0700

    feat(blog): Implement Netlify CMS for post-writing

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,3 +37,13 @@ export default function Home() {
     </Layout>
   );
 }
+
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on('init', (user) => {
+    if (!user) {
+      window.netlifyIdentity.on('login', () => {
+        document.location.href = '/admin/';
+      });
+    }
+  });
+}
